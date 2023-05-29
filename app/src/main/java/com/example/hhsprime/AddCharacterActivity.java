0c7d6215139837a762f82cf36cdb1e8f702afdc5
Bas Apon <apon.bas@gmail.com>
2023-05-30 00:04:22 +0200

    first commit

---
+++ app/src/main/java/com/example/hhsprime/AddCharacterActivity.java
@@ -0,0 +1,14 @@
+package com.example.hhsprime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddCharacterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_character);
+    }
+}
